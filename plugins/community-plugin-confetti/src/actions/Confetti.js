201281a88e26ba99c4a64ff26dec1065e4d99452
Sam Tolmay <sam@lowdefy.com>
2023-09-21 16:46:56 +0200

    fix: Remove uneeded default values.

--- plugins/community-plugin-confetti/src/actions/Confetti.js
+++ plugins/community-plugin-confetti/src/actions/Confetti.js
@@ -2,13 +2,7 @@ import JSConfetti from 'js-confetti';
 
 async function Confetti({ params = {} }) {
   const jsConfetti = new JSConfetti();
-  const {
-    emojis,
-    confettiRadius = 3,
-    confettiNumber = 50,
-    emojiSize = 10,
-    confettiColors,
-  } = params;
+  const { emojis, confettiRadius, confettiNumber, emojiSize, confettiColors } = params;
   jsConfetti.addConfetti({
     emojis,
     confettiRadius,
