052a2f5d0dd9614f6f7294dc5b350d8a61f8e46d
Sam Tolmay <sam@lowdefy.com>
2023-09-21 16:32:28 +0200

    feat: Updates to confetti plugin.

--- plugins/community-plugin-confetti/src/actions/Confetti.js
+++ plugins/community-plugin-confetti/src/actions/Confetti.js
@@ -1,16 +1,21 @@
 import JSConfetti from 'js-confetti';
 
-async function Confetti({ params }) {
-    const jsConfetti = new JSConfetti();
-    const { emojis = ['🌈', '⚡️'], confettiRadius = 3, confettiNumber = 50, emojiSize = 10, confettiColors= [
-        '#ff0a54', '#ff477e'] } = params;
-    jsConfetti.addConfetti({
+async function Confetti({ params = {} }) {
+  const jsConfetti = new JSConfetti();
+  const {
+    emojis,
+    confettiRadius = 3,
+    confettiNumber = 50,
+    emojiSize = 10,
+    confettiColors,
+  } = params;
+  jsConfetti.addConfetti({
     emojis,
     confettiRadius,
     confettiNumber,
     emojiSize,
-    confettiColors
-    });
+    confettiColors,
+  });
 }
 
 export default Confetti;
