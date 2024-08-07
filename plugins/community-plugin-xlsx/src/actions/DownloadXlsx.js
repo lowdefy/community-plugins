ea714a96e70be4170497a9347ab2c70f1f479832
Salahuddin Saiet <salahuddin@lowdefy.com>
2024-08-07 10:51:25 +0200

    feat: Excel Download package update.
    Added functionality for dot-notation in the value field.
    Added functionality for Array types.

--- plugins/community-plugin-xlsx/src/actions/DownloadXlsx.js
+++ plugins/community-plugin-xlsx/src/actions/DownloadXlsx.js
@@ -1,33 +1,38 @@
-import { type } from '@lowdefy/helpers';
+import { type, get } from '@lowdefy/helpers';
 import writeXlsxFile from 'write-excel-file';
 
-async function DownloadXlsx({ params }) {
-  const { data, fileName, schema, ...options } = params;
-
-  if (!type.isArray(data) || !type.isObject(data[0])) {
-    throw new Error('Data should be an array of objects.');
+const createGetValue = ({ column }) => {
+  if (type.isString(column.value)) {
+    if (column.type === 'Array') {
+      return (row) => get(row, column.value).filter(Boolean).join(',');
+    }
+    return (row) => get(row, column.value);
   }
+  return column.value;
+}
 
-  const colTypes = {
-    String: String,
-    Number: Number,
-    Boolean: Boolean,
-    Date: Date,
-  };
-
-  await writeXlsxFile(data, {
-    fileName: !type.isString(fileName) ? 'download.xlsx' : fileName,
-    schema:
-      type.isArray(schema) &&
-      schema.map((column) => ({
-        ...column,
-        value: type.isString(column.value) ? (row) => row[column.value] : column.value,
-        type: type.isString(column.type) ? colTypes[column.type] ?? column.type : undefined,
-      })),
-    ...options,
-  });
-
-  return;
+async function DownloadXlsx({ params  }) {
+    const { data , fileName , schema , ...options } = params;
+    if (!type.isArray(data) || !type.isObject(data[0])) {
+        throw new Error('Data should be an array of objects.');
+    }
+    const colTypes = {
+        String: String,
+        Number: Number,
+        Boolean: Boolean,
+        Date: Date,
+        Array: String
+    };
+    await writeXlsxFile(data, {
+        fileName: !type.isString(fileName) ? 'download.xlsx' : fileName,
+        schema: type.isArray(schema) && schema.map((column)=>({
+                ...column,
+                value: createGetValue({ column }),
+                type: type.isString(column.type) ? get(colTypes, column.type) ?? column.type : undefined
+            })),
+        ...options
+    });
+    return;
 }
 
 export default DownloadXlsx;
