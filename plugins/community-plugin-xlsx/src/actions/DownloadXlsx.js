36b3f68e5f9b73f7c4bf562f2244300a9283773f
Sam Tolmay <sam@lowdefy.com>
2024-08-08 10:29:31 +0200

    feat: Updates to DownloadXlsx action.

--- plugins/community-plugin-xlsx/src/actions/DownloadXlsx.js
+++ plugins/community-plugin-xlsx/src/actions/DownloadXlsx.js
@@ -1,38 +1,42 @@
 import { type, get } from '@lowdefy/helpers';
 import writeXlsxFile from 'write-excel-file';
 
-const createGetValue = ({ column }) => {
-  if (type.isString(column.value)) {
-    if (column.type === 'Array') {
-      return (row) => get(row, column.value)?.filter(Boolean).join(', ');
-    }
-    return (row) => get(row, column.value);
+function createValueFunction(column) {
+  if (type.isFunction(column.value)) {
+    return column.value;
   }
-  return column.value;
+  if (column.type === 'Array') {
+    return (row) => get(row, column.value)?.filter(Boolean).join(', ');
+  }
+  return (row) => get(row, column.value);
 }
 
-async function DownloadXlsx({ params  }) {
-    const { data , fileName , schema , ...options } = params;
-    if (!type.isArray(data) || !type.isObject(data[0])) {
-        throw new Error('Data should be an array of objects.');
-    }
-    const colTypes = {
-        String: String,
-        Number: Number,
-        Boolean: Boolean,
-        Date: Date,
-        Array: String
-    };
-    await writeXlsxFile(data, {
-        fileName: !type.isString(fileName) ? 'download.xlsx' : fileName,
-        schema: type.isArray(schema) && schema.map((column)=>({
-                ...column,
-                value: createGetValue({ column }),
-                type: type.isString(column.type) ? get(colTypes, column.type) ?? column.type : undefined
-            })),
-        ...options
-    });
-    return;
+async function DownloadXlsx({ params }) {
+  const { data, fileName, schema, ...options } = params;
+  if (!type.isArray(data) || !type.isObject(data[0])) {
+    throw new Error('Data should be an array of objects.');
+  }
+  if (!type.isArray(schema) || !type.isObject(schema[0])) {
+    throw new Error('Schema should be an array of objects.');
+  }
+  const colTypes = {
+    String: String,
+    Number: Number,
+    Boolean: Boolean,
+    Date: Date,
+    Array: String,
+  };
+
+  await writeXlsxFile(data, {
+    fileName: !type.isString(fileName) ? 'download.xlsx' : fileName,
+    schema: schema.map((column) => ({
+      ...column,
+      value: createValueFunction(column),
+      type: get(colTypes, column.type) ?? column.type,
+    })),
+    ...options,
+  });
+  return;
 }
 
 export default DownloadXlsx;
